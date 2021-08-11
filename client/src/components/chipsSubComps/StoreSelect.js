ffdafe93e122cba46e0227689b3d9e5e42e19569
Ian Wren <ian.wren81@gmail.com>
2021-08-11 16:42:10 -0700

    Deleted unnecessary code and files for refactor

--- client/src/components/chipsSubComps/StoreSelect.js
+++ client/src/components/chipsSubComps/StoreSelect.js
@@ -1,30 +1,56 @@
-import React from 'react'
-import { Card, Container, Accordion, Button } from 'react-bootstrap'
-import NavbarComponent from "../SharedComponents/Navbar"
+import React, { useRef, useState } from 'react'
 
+import Local from "../../utils/localStorage"
+// import { useAuth } from '../../contexts/AuthContext'
+import { Form } from "react-bootstrap"
 
 export default function StoreSelect() {
+
+   
+    // const { currentUser } = useAuth()
+    const chipArr = Local.getChipsArr()
+    const chipNameRef = useRef()
+    console.log(chipArr)
+ 
+
+
+    function handleAddChart() {
+
+        
+        const targetChip = chipArr.filter(chip => chip.name === chipNameRef.current.value)
+        console.log(targetChip[0].doses)
+      
+     
+    }
+  
+    const data = {
+        // dates = lables
+        labels: dates,
+        datasets: [
+            {
+                // label: [chipNameRef.current.value],
+                // put in the blood sugars 
+                data: tests,
+                fill: false,
+                backgroundColor: 'rgb(207, 4, 47)',
+                borderColor: 'rgba(255, 99, 132, 0.2)',
+            },
+        ],
+    }
+    
+    
+   
     return (
         <div>
-            <NavbarComponent />
-            
-            <Accordion defaultActiveKey="0">
-                <Card>
-                    <Card.Header>
-                        <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                            Choose Store
-                        </Accordion.Toggle>
-                    </Card.Header>
-                    <Accordion.Collapse eventKey="0">
-                        <Card.Body>Choose Store to Update Chip List</Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-            </Accordion>
-            
-            <Container>
-                
-            </Container>
-
+            <Form>
+                <Form.Group>
+                    <Form.Label>Store Name</Form.Label>
+                    <Form.Control as="select" ref={chipNameRef} onChange={handleAddChart}>
+                        {chipArr.map(chip => (<option>{chip.name}</option>))}
+                    </Form.Control>
+                </Form.Group>
+            </Form>
+            {activeChart && <Line data={data} options={options} />}
         </div>
     )
 }
