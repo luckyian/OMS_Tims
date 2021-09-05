319f9c853752c28eaf61d3f855858df10b388f01
Mykel Valadez <valadezmykel@gmail.com>
2021-09-05 13:07:15 -0700

    added in function to handle adding and getting all chips from the flat file

--- routes/api/chips.js
+++ routes/api/chips.js
@@ -1,11 +1,41 @@
 const router = require("express").Router();
+const path = require("path");
+const fs = require("fs")
 const chipsController = require('../../controller/chipsController')
-// order = dose
-// orderCase = takeMedication
+
+// api/chips
 router.route("/")
     .delete(chipsController.removeChip)
-    .post(chipsController.addNewChip)
+    // .post(chipsController.addNewChip)
+
+    // this code was made to work with the flat file json
+    // this will send a json file with all the chips data 
+    .get((req, res) => {
+        res.json(path.join(__dirname, "./schema/chips.json"))
+
+    })
+    // this will replace the json db with a newly written db
+    .post((req, res) => {
+        let chips = require(path.join(__dirname, "./schema/chips.json"))
+        // the request here should be an object formatted the same as the db 
+        chips.push(req)
+
+        // writes a new file (path, data, callback)
+        fs.writeFile(path.join(__dirname, "./schema/chips.json"), JSON.stringify(chips), err => {
+            if(err){
+                console.log(err)
+                res.status("500")
+            }
+
+            res.status("200")
+        })
+    })  
+
+
+
+// api/chips/chip
 router.route("/chip")
     .post(chipsController.orderCase)
+
 
 module.exports = router;
