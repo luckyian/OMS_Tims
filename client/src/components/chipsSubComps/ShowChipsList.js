888e253782d903c425bf6fc5594cdb944abdb4da
Ian Wren <ian.wren81@gmail.com>
2021-10-20 20:40:19 -0700

    Added code for viewing chips list

---
+++ client/src/components/chipsSubComps/ShowChipsList.js
@@ -0,0 +1,18 @@
+import React, {useRef} from 'react'
+import { Form } from 'react-bootstrap'
+import stores from '../../reference/stores.json'
+import chips from '../../reference/chips.json'
+// import Local from '../../../utils/localStorage'
+import Local from '../../utils/localStorage'
+export default function ShowChipList() {
+
+
+    const chipsRef = useRef()
+    let allChips = chips
+
+    return (
+<div>
+    
+</div>
+    )
+}
