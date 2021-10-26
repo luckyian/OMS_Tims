0a7379bade0c4c9616a38e52b50e9ec44fece883
Ian Wren <ian.wren81@gmail.com>
2021-10-25 23:40:10 -0700

    Added sample code to use as base for rendering chips list with .map

--- client/src/components/chipsSubComps/ShowChipsList.js
+++ client/src/components/chipsSubComps/ShowChipsList.js
@@ -12,7 +12,63 @@ export default function ShowChipList() {
 
     return (
 <div>
+     {/* <script>
+     let chipSet = []
+    let highScores = [];
     
+    chipSet = JSON.parse(localStorage.getItem("allChips")) || [];
+    
+    console.log(chipSet);
+    
+    let highScoresListEl = document.querySelector(".high-score-list");
+    let chipSetList = document.querySelector(".chips-list");
+    
+    function cityList() {
+    cityArr.forEach(function (city) {
+      let tRow = $(`<button type="button" class="btn btn-primary city-btn" id="${city}">${city}</button>`);
+      $(".listie").append(tRow);
+
+    })
+
+  };
+  cityList();
+      
+  function renderChips() {
+        allChips.forEach(
+          
+        )
+      }
+
+    function renderHighScores() {
+
+    
+
+      // Render a new li for each high score
+      for (var i = 0; i < highScores.length; i++) {
+        let scores = highScores[i];
+
+        var li = document.createElement("li");
+        li.textContent = highScores[i].initials + "   " + highScores[i].score;
+
+
+        highScoresListEl.appendChild(li);
+      }
+    };
+
+    function init() {
+      
+      // Parsing the JSON string to an object
+      let storedScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+      // If scores were retrieved from localStorage, update the scores array to it
+      if (storedScores !== null) {
+        highScores = storedScores;
+      }
+
+      // Render todos to the DOM
+      renderHighScores();
+    };
+    init(); */}
 </div>
     )
 }
